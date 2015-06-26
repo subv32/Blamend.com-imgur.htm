e4441a2d91682678ef337ce5b29ff96f19fa87fa
subv32 <subv32@national.shitposting.agency>
2015-06-26 12:15:35 -0900

    added some sanitizing.. ew actual php

--- create.php
+++ create.php
@@ -1,15 +1,17 @@
 <?php
-
-shell_exec("sed s/'<\/body><\/html>'\/\/ imgur.htm > imgur.htm2; mv imgur.htm2 imgur.htm");
-
 $urla = escapeshellarg($_GET["url"]);
-$command = "echo '<a href=" . $urla . "><img src=" . $urla . " height=250 width=250/></a>' >> imgur.htm";
+$urla = trim($urla);
+echo $urla;
+if (strpos($urla,'imgur.com/') !== false && substr($urla,0,5) == '\'http') {
+        shell_exec("sed s/'<\/body><\/html>'\/\/ imgur.htm > imgur.htm2; mv imgur.htm2 imgur.htm");
+        $command = "echo '<a href=" . $urla . "><img src=" . $urla . " height=250 width=250/></a>' >> imgur.htm";
 
-//<a href="https://i.imgur.com/SBZTpLf.jpg/"><img src=https://i.imgur.com/SBZTpLf.jpg/ height=250 width=250></a>
+        //<a href="https://i.imgur.com/SBZTpLf.jpg/"><img src=https://i.imgur.com/SBZTpLf.jpg/ height=250 width=250></a>
 
-shell_exec($command);
+        shell_exec($command);
 
-shell_exec("echo '</body></html>' >> imgur.htm");
+        shell_exec("echo '</body></html>' >> imgur.htm");
+}
 
 // Loggging.. 
 $ip = getenv('HTTP_CLIENT_IP')?:
@@ -22,5 +24,3 @@ getenv('REMOTE_ADDR');
 shell_exec("echo " . escapeshellarg($ip) . " sent us " . $urla . " >> ip.log");
 
 ?>
-
-
