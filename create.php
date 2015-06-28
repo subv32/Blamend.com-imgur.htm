55ccbf1191e3ac0ee947dea6d8989267f444742b
subv32 <subv32@national.shitposting.agency>
2015-06-28 02:55:45 -0400

    now uses imgur.db

--- create.php
+++ create.php
@@ -1,15 +1,9 @@
 <?php
 $urla = escapeshellarg($_GET["url"]);
 $urla = trim($urla);
+echo $urla;
 if (strpos($urla,'imgur.com/') !== false && substr($urla,0,5) == '\'http') {
-        shell_exec("sed s/'<\/body><\/html>'\/\/ imgur.htm > imgur.htm2; mv imgur.htm2 imgur.htm");
-        $command = "echo '<a href=" . $urla . "><img src=" . $urla . " height=250 width=250/></a>' >> imgur.htm";
-
-        //<a href="https://i.imgur.com/SBZTpLf.jpg/"><img src=https://i.imgur.com/SBZTpLf.jpg/ height=250 width=250></a>
-
-        shell_exec($command);
-
-        shell_exec("echo '</body></html>' >> imgur.htm");
+	shell_exec("echo " . $urla . " >> imgur.db");
 }
 
 // Loggging.. 
