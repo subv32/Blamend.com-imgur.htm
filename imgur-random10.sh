c2fe1233a36fa244de7c2ea13ccdcb89f9baa3b9
subv32 <subv32@national.shitposting.agency>
2015-06-28 09:24:45 -0900

    depreciated

--- imgur-random10.sh
+++
@@ -1,2 +0,0 @@
-urls=$(cat imgur.db | grep imgur | sed s/"<a href="// | cut -d">" -f1 | sort -R | head -10 )
-for i in $(echo -e "$urls"); do echo "<a href=$i><img src=$i height=300 width=300></a>"; done
